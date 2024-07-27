6c6e1fb797c5ef8986e0b8483f71afcfe18c1b17
Jihun <58257616+Lamyzm@users.noreply.github.com>
2024-07-27 22:29:35 +0900

    chore: nextjs 기본 설정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
-import { auth } from '@/auth';
 
 const secret = process.env.AUTH_SECRET as string;
 export async function middleware(request: NextRequest) {
