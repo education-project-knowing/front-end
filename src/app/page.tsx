c66b2f36c425b73fabe879668b5fbe3ff13c4056
Jihun <58257616+Lamyzm@users.noreply.github.com>
2024-07-27 23:07:17 +0900

    test: 자동배포 test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,50 @@
 'use client';
+import { useTheme } from 'next-themes';
+import { useEffect } from 'react';
+import { Button } from '@/components/ui/button';
 
-import { getProviders, signIn } from 'next-auth/react';
+const usePageLeaveWarning = (message: any) => {
+  useEffect(() => {
+    const handleWindowClose = (e: any) => {
+      e.preventDefault();
+      e.returnValue = message;
+      return message;
+    };
+
+    window.addEventListener('beforeunload', handleWindowClose);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleWindowClose);
+    };
+  }, [message]);
+};
 
 export default function Home() {
+  const { theme, setTheme } = useTheme();
+  usePageLeaveWarning('이 페이지를 벗어나면 변경 사항이 저장되지 않습니다. 계속하시겠습니까?');
+
+  console.log(theme);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>안녕하시지!!!</h1>
-      <form
-        action={async () => {
-          await signIn('naver');
-        }}>
+      <h1>안녕하시지!!!!</h1>
+      <h1>안녕하세요 2트</h1>
+      <form>
         <button type="submit">Signin with Naver </button>
       </form>
       <div className="flex flex-col p-10">
-        <button onClick={() => signIn('kakao', { redirect: true, callbackUrl: '/' })}>카카오 로그인</button>
-        <button onClick={() => signIn('naver')}>네이버 로그인</button>
+        <button>카카오 로그인</button>
+        <button>네이버 로그인</button>
       </div>
+      <Button>hi</Button>
+      <select
+        className="bg-background"
+        value={theme}
+        onChange={e => setTheme(e.target.value)}>
+        <option value="system">System</option>
+        <option value="dark">Dark</option>
+        <option value="light">Light</option>
+        <option value="blue">blue</option>
+      </select>
     </main>
   );
 }
