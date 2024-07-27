3a06b4b36bb59ff7ee4373438639f163a9f337f9
Jihun <58257616+Lamyzm@users.noreply.github.com>
2024-07-27 22:21:03 +0900

    chore: 기본 패키지 설치

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Svg loader
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
