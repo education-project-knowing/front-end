175b514e1b194a2a19888e8d41654a8ff96f1fd0
Jihun <58257616+Lamyzm@users.noreply.github.com>
2024-08-01 22:49:36 +0900

    feat: / => main middleware 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,10 @@ import { getToken } from 'next-auth/jwt';
 const secret = process.env.AUTH_SECRET as string;
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
+
+  if (pathname === '/') {
+    return NextResponse.redirect(new URL('/main', request.url));
+  }
 }
 
 export const config = {
