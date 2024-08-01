eb7e007662aa4ce504eda6f2101908c82ac3e9eb
Jihun <58257616+Lamyzm@users.noreply.github.com>
2024-08-01 13:46:43 +0900

    test: git action

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+rm -rf output  # 기존 output 디렉토리가 있다면 제거
 mkdir output
-cp -R ./* ./output/
+find . -maxdepth 1 -not -name 'output' -not -name '.' -not -name '..' -exec cp -R {} output/ \;
