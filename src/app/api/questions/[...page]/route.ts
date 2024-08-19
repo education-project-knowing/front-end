a4f558cd1c06e2a5d4be26b60625438382d7fd79
Jihun <58257616+Lamyzm@users.noreply.github.com>
2024-08-19 15:30:29 +0900

    add: api router 설정

--- src/app/api/questions/[...page]/route.ts
+++ src/app/api/questions/[...page]/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
+
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const page = searchParams.get('page') || '1';
