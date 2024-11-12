d3f8c84fab427e8e0b50abbe72aee9e18d84182c
Jihun <58257616+Lamyzm@users.noreply.github.com>
2024-11-13 01:27:33 +0900

    chore:의존성 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Svg loader
     config.module.rules.push({
+      // Svg loader 설정
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
     });
 
     return config;
+  },
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.API_BASE_URL}/:path*`,
+      },
+    ];
   },
 };
 
